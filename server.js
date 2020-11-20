1070c3479a17a1091eadf42f91197e62c89dbe15
Aivorlan <aivorlan@icloud.com>
2020-11-19 23:43:58 -0600

    Setting up port connection

--- server.js
+++ server.js
@@ -3,6 +3,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require('console.table');
-
+const path = require('path');
 require('dotenv').config();
+
+//Port connection 
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "4971645Jh!",
+    database: "employees_db"
+  });
+
+  
 
