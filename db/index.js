2d693f00359fa3be00c6634d0e5b163c9e95119b
Aivorlan <aivorlan@icloud.com>
2020-12-17 23:07:04 -0600

    Updating file

--- db/index.js
+++ db/index.js
@@ -7,8 +7,8 @@ const db = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '4971645Jh!',
-  database: 'emp_cmsa_db'
+  password: '',
+  database: ''
 })
 
 module.exports = db
